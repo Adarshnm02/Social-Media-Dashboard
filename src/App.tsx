e85935a1e240d343fee8ef28b35347337d1c6965
Adarsh nm <adarshnm02@gmail.com>
2025-01-30 17:59:31 +0530

    First commit after initializing the project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="flex text-center justify-center min-h-screen items-center ">
+      <h1 className="bg-amber-900 text-5xl hover:bg-blue-950 p-4 rounded-4xl"> Hello World</h1>
+    </div>
+  )
+}
+
+export default App
