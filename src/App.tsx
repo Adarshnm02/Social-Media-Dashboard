37b2bffd4d3b71a4aab98335a2258e74d4581e27
Adarsh nm <adarshnm02@gmail.com>
2025-01-31 00:28:31 +0530

    Add protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { BrowserRouter, Navigate, Outlet, Route, Routes } from "react-router-dom"
+import Login from "./pages/Login"
+import Register from "./pages/Register"
+import Home from "./pages/Home"
+import { useSelector } from "react-redux"
+import { RootState } from "./app/store"
+
+const ProtectedRoute = () => {
+  const { token } = useSelector((state: RootState) => state.auth);
+  return token ? <Outlet /> : <Navigate to="/login" replace />
+}
 
 const App = () => {
   return (
-    <div className="flex text-center justify-center min-h-screen items-center ">
-      <h1 className="bg-amber-900 text-5xl hover:bg-blue-950 p-4 rounded-4xl"> Hello World</h1>
-    </div>
+    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>} />
+        <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
