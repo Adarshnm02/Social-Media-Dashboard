287d4ceb2d2b52b311bc441508f443eaf6dbc91a
Adarsh nm <adarshnm02@gmail.com>
2025-01-31 09:38:34 +0530

    Connect socket.io to Redux auth state

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,4 +1,5 @@
 import io from 'socket.io-client';
+import { store } from '../app/store';
 
 const socket = io('http://localhost:3001', {
   autoConnect: false,
@@ -8,5 +9,13 @@ export const connectSocket = (token: string) => {
     socket.io.opts.auth = { token };
   socket.connect();
 };
+
+store.subscribe(() => {
+  const token = store.getState().auth.token;
+  if(token && !socket.connected){
+    socket.io.opts.auth = { token }
+    socket.connect();
+  }
+})
 
 export default socket;
